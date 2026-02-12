0bd1d2a59a62a55bb5aa98ed748d6080d9a04ce6
boazmr <boazmr112@gmail.com>
2026-02-12 10:22:05 +0200

    Encapsulate some functions

--- FinalProject/code/utils.py
+++ FinalProject/code/utils.py
@@ -2,14 +2,61 @@
 
 from PIL import Image
 from torchvision import transforms
+from transformers import CLIPTokenizer, CLIPTextModel
+from diffusers import DDIMScheduler, AutoencoderKL, UNet2DConditionModel
 import torch
 
-def image_to_tensor(image_path, tensor_size):
+
+def create_pipeline_components(model_id, device, dtype):
+    """
+    Create a pipeline from a hugging face model path.
+    Specifically designed for the project model.
+    The pipeline components will be:
+    Tokenizer, Text Encoder, Unet, VAE, Scheduler.
+    Return a dictionary of (name, element)
+    :param model_id: The diffusion model in use.
+    :param device: The device to run the models on.
+    :param dtype: The torch dtype to use.
+    """
+    components_dict = {}
+    components_dict["tokenizer"] = CLIPTokenizer.from_pretrained(
+        model_id,
+        subfolder="tokenizer"
+    )
+    
+    components_dict["text_encoder"] = CLIPTextModel.from_pretrained(
+        model_id,
+        subfolder="text_encoder",
+        torch_dtype=dtype
+    ).to(device)
+
+    components_dict["unet"] = UNet2DConditionModel.from_pretrained(
+        model_id,
+        subfolder="unet",
+        torch_dtype=dtype
+    ).to(device)
+    
+    components_dict["vae"] = AutoencoderKL.from_pretrained(
+        model_id,
+        subfolder="vae",
+        torch_dtype=dtype
+    ).to(device)
+
+    components_dict["scheduler"] = DDIMScheduler.from_pretrained(
+        model_id,
+        subfolder="scheduler"
+    )
+
+    return components_dict
+
+def image_to_tensor(image_path, tensor_size, device, dtype):
     """
     Transform image to a normalized tensor of size (1, 3, tensor_size, tensor_size).
     
     :param image_path: Local path to image.
     :param tensor_size: Desired size of output tensor.
+    :param device: The device to move the tensor to ('cuda' or 'cpu').
+    :param dtype: The torch dtype to cast the tensor to.
     """
     transform = transforms.Compose([
         transforms.Resize((tensor_size, tensor_size)),
@@ -17,19 +64,47 @@ def image_to_tensor(image_path, tensor_size):
         transforms.Normalize([0.5], [0.5])  # Normalize to cope with autoencoder value range
     ])
     img = Image.open(image_path).convert("RGB")
-    return transform(img).unsqueeze(0)
-
+    img_tensor = transform(img).unsqueeze(0)
+    img_tensor = img_tensor.to( # Move image_tensor to GPU, and transform to dtype = torch.float16
+        device=device,
+        dtype=dtype
+    )
+    return img_tensor
+    
 def tensor_to_image(image_tensor, saving_path):
     """
     Transform tensor to an image and saves it in given path.
     Specifically designated to transform VAE decoder output to an image.
 
-    :param image_tensor: The image tensor.
+    :param image_tensor: The image tensor, expected in BCHW format.
     :param saving_path: The path to save the image.
     """
-    image = (image_tensor / 2 + 0.5).clamp(0, 1)
-    image = (image * 255).round().to(torch.uint8)
-    image = image.cpu().permute(0, 2, 3, 1)  # BCHW → BHWC
-    image = image[0].numpy()
+    image = image_tensor[0].detach().cpu().float() # Select first image in batch, move to CPU, and convert to float32.
+    image = (image / 2 + 0.5).clamp(0, 1) # De-normalize from [-1, 1] to [0, 1]
+    image = image.permute(1, 2, 0)        # Permute from CHW to HWC
+    image = (image * 255).round().to(torch.uint8).numpy() # Scale to [0, 255]
     image = Image.fromarray(image)
     image.save(saving_path)
+
+def get_text_embeddings(prompt, tokenizer, text_encoder, device):
+    """
+    Create tokens from prompt, then create an embedding of these tokens.
+
+    :param prompt: The text prompt to embed.
+    :param tokenizer: The tokenizer model.
+    :param text_encoder: The text encoder model.
+    :param device: The device to run the models on.
+    :return: The generated text embeddings.
+    """
+    # Tokenize the prompt
+    text_inputs = tokenizer(
+        prompt,
+        padding="max_length",
+        max_length=tokenizer.model_max_length,
+        truncation=True,
+        return_tensors="pt",
+    )
+    # Transform tokens to embedding
+    with torch.no_grad():
+        embeddings = text_encoder(text_inputs.input_ids.to(device))[0]
+    return embeddings
--- FinalProject/code/vanilla_inpainting.py
+++ FinalProject/code/vanilla_inpainting.py
@@ -15,65 +15,25 @@ import utils
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 dtype = torch.float16
 
+# Set up pathes
 model_id = "sd2-community/stable-diffusion-2-base"
+image_path = "../images/image01.jpg"
+mask_path = "../images/mask01.jpg"
 
-# Setting up pipeline components:
-tokenizer = CLIPTokenizer.from_pretrained(
-    model_id,
-    subfolder="tokenizer"
-)
-
-text_encoder = CLIPTextModel.from_pretrained(
-    model_id,
-    subfolder="text_encoder",
-    torch_dtype=dtype
-).to(device)
-
-unet = UNet2DConditionModel.from_pretrained(
-    model_id,
-    subfolder="unet",
-    torch_dtype=dtype
-).to(device)
-
-vae = AutoencoderKL.from_pretrained(
-    model_id,
-    subfolder="vae",
-    torch_dtype=dtype
-).to(device)
-
-scheduler = DDIMScheduler.from_pretrained(
-    model_id,
-    subfolder="scheduler"
-)
+# Set up pipeline components
+pipe_comp_dict = utils.create_pipeline_components(model_id, device, dtype)
+tokenizer = pipe_comp_dict["tokenizer"]
+text_encoder = pipe_comp_dict["text_encoder"]
+unet = pipe_comp_dict["unet"]
+vae = pipe_comp_dict["vae"]
+scheduler = pipe_comp_dict["scheduler"]
 
 prompt = "Portrait of a beautiful woman sitting at a Parisian cafe table, white sweater, holding a coffee cup, soft sunlight, blurred street background, high resolution, highly detailed skin, 8k, cinematic lighting."
 
-# Tokenize the prompt
-text_inputs = tokenizer(
-    prompt,
-    padding="max_length",
-    max_length=tokenizer.model_max_length,
-    truncation=True,
-    return_tensors="pt",
-)
-# Tokenize an empty prompt for the Classifier Free Guidance
-uncond_text = tokenizer(
-    "",
-    padding="max_length",
-    max_length=tokenizer.model_max_length,
-    truncation=True,
-    return_tensors="pt",
-)
-# Transform tokens to embedding
-with torch.no_grad():
-    text_embeddings = text_encoder(
-        text_inputs.input_ids.to(device)
-    )[0]
-# Create unconditional embeddings for Classifier Free Guidance. That is, create embeddings for an empy prompt.
-with torch.no_grad():
-    uncond_embeddings = text_encoder(
-        uncond_text.input_ids.to(device)
-    )[0]
+# Embedd the prompt. workflow: prompt -> tokens -> embedding
+text_embeddings = utils.get_text_embeddings(prompt, tokenizer,text_encoder, device)
+# Embedd an empty prompt for the Classifier Free Guidance
+uncond_embeddings = utils.get_text_embeddings("", tokenizer,text_encoder, device)
 
 # Sample random latent space data
 batch_size = 1
@@ -87,22 +47,20 @@ latents = torch.randn(
     ),
     device=device,
     dtype=dtype,
-)
+) * scheduler.init_noise_sigma # Scale the noisy latent by the scheduler scalar, as it was trained on
 
-image_path = "image.jpg"
-mask_path = "mask.jpg"
+# Transform image to torch tensor and then to latent space
+vae_sample_size = vae.config.sample_size #  size of feature space
+img_tensor = utils.image_to_tensor(image_path=image_path, tensor_size=vae_sample_size, device=device, dtype=dtype) # Image to tensor
+with torch.no_grad():  # Transform image_tensor to latent space representation
+    latent_space_image = vae.encode(img_tensor).latent_dist.sample() * vae.config.scaling_factor
 
-# 1) Transform image to torch and then to latent space by using the encoder
-vae_sample_size = vae.config.sample_size #  size of fature space
-img_tensor = utils.image_to_tensor(image_path=image_path, tensor_size=vae_sample_size)
+# Transform mask to latent space size torch tensor. Not using autoencoder.
+mask_latent_tensor = utils.image_to_tensor(image_path=mask_path, tensor_size=latent_sample_size, device=device, dtype=dtype)
 
-# 2) Transform mask to latent space size torch tensor. Not using autoencoder.
-mask_latent_tensor = utils.image_to_tensor(image_path=mask_path, tensor_size=latent_sample_size)
-    
 # 3) Each step apply multiplication between the mask and the diffusion step
 
 scheduler.set_timesteps(50)
-latents = latents * scheduler.init_noise_sigma # Scale the noisy latent by the scheduler scalar, as it was trained on
 guidance_scale = 5 # Control the CFG. Higher values -> higher dependency on the prompt, more noise
 
 # Applying the diffusion iterations.
@@ -135,4 +93,3 @@ with torch.no_grad():
     image = vae.decode(latents).sample
 
 utils.tensor_to_image(image_tensor=image, saving_path="generated.png")
-
