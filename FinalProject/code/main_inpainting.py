eff5e16e2a72e0b6b84342bf02ab76dce55db787
Boaz Maron <boazmr112@gmail.com>
2026-03-08 16:20:16 +0200

    optimization update

--- FinalProject/code/main_inpainting.py
+++ FinalProject/code/main_inpainting.py
@@ -11,6 +11,7 @@ import numpy as np
 from torchvision import transforms
 import argparse
 import utils
+import torch.nn.functional as F
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description="Run inpainting with specified image number")
@@ -23,23 +24,16 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 dtype = torch.float16
 model_id = "sd2-community/stable-diffusion-2-base"
 
-# Set up pathes
-image_path = "../data/images/image0" + str(image_num) + ".jpg"
-mask_path = "../data/masks/mask0" + str(image_num) + ".jpg"
-saving_output_path = "../data/outputs/main_outputs/inpaint0" + str(image_num) + ".jpg"
+# Set up data pathes
+image_path, mask_path, saving_output_path = utils.get_paths(image_num)
 
-with open("../data/prompts.txt", "r") as f:
-    prompts = f.read().splitlines()
-
-prompt = prompts[image_num-1]
+# Get prompt number 'image_num' from "prompts.txt" file.  
+prompt = utils.get_prompt(image_num)
 
 # Set up pipeline components
-pipe_comp_dict = utils.create_pipeline_components(model_id, device, dtype)
-tokenizer = pipe_comp_dict["tokenizer"]
-text_encoder = pipe_comp_dict["text_encoder"]
-unet = pipe_comp_dict["unet"]
-vae = pipe_comp_dict["vae"]
-scheduler = pipe_comp_dict["scheduler"]
+pipe = utils.create_pipeline_components(model_id, device, dtype)
+tokenizer, text_encoder = pipe["tokenizer"], pipe["text_encoder"]
+unet, vae, scheduler = pipe["unet"], pipe["vae"], pipe["scheduler"]
 
 # Set up loss function for latent space
 loss_fn = utils.LatentLoss()
@@ -49,81 +43,117 @@ text_embeddings = utils.get_text_embeddings(prompt, tokenizer,text_encoder, devi
 # Embedd an empty prompt for the Classifier Free Guidance
 uncond_embeddings = utils.get_text_embeddings("", tokenizer,text_encoder, device)
 
-# Sample random latent space data
-batch_size = 1
-latent_sample_size = unet.config.sample_size
-latents = torch.randn(
-    (
-        batch_size,
-        unet.config.in_channels,
-        latent_sample_size, # Height = latent sample size
-        latent_sample_size, # Widrh = latent sample size
-    ),
-    device=device,
-    dtype=dtype,
-) * scheduler.init_noise_sigma # Scale the noisy latent by the scheduler scalar, as it was trained on
+# Create a list of size 'num_of_inpaintings' of images. 
+images = []
+num_of_inpaintings = 1
 
-# Transform image to torch tensor and then to latent space
-vae_sample_size = vae.config.sample_size #  size of feature space
-img_tensor = utils.image_to_tensor(image_path=image_path, tensor_size=vae_sample_size, is_mask=False, device=device, dtype=dtype) # Image to tensor
-with torch.no_grad():  # Transform image_tensor to latent space representation
-    latent_original_image = vae.encode(img_tensor).latent_dist.sample() * vae.config.scaling_factor
+for inpaint_iteration in range(num_of_inpaintings):
+    # Sample random latent space data
+    batch_size = 1
+    latent_sample_size = unet.config.sample_size
+    latents = torch.randn(
+        (
+            batch_size,
+            unet.config.in_channels,
+            latent_sample_size, # Height = latent sample size
+            latent_sample_size, # Widrh = latent sample size
+        ),
+        device=device,
+        dtype=dtype,
+    ) * scheduler.init_noise_sigma # Scale the noisy latent by the scheduler scalar, as it was trained on
 
-# Transform mask to latent space size binary torch tensor based on the masked region.
-mask_latent_tensor = utils.image_to_tensor(image_path=mask_path, tensor_size=latent_sample_size, is_mask=True, device=device, dtype=dtype)
+    # Transform image:
+    # Image -> torch tensor with filled mask areas -> latent space
+    vae_sample_size = vae.config.sample_size #  size of pixel space
+    img_tensor = utils.image_fill_mask_boundary_average(image_path=image_path, mask_path=mask_path, tensor_size=vae_sample_size, blur_radius=0, device=device, dtype=dtype) # Image to tensor
+    with torch.no_grad():  # Transform image_tensor to latent space representation
+        latent_original_image = vae.encode(img_tensor).latent_dist.sample() * vae.config.scaling_factor
 
-scheduler.set_timesteps(25)
-guidance_scale = 7 # Control the CFG. Higher values -> higher dependency on the prompt. Values should be around 6-12.
-optimization_scale = 0.1
+    # Transform mask to latent space size binary torch tensor based on the masked region.
+    mask_latent, mask_edge_latent = utils.mask_to_tensor(mask_path=mask_path, tensor_size=latent_sample_size, blur_radius=0, mask_shrink=0, device=device, dtype=dtype)
 
-# Generate noise once with fixed seed for reproducible inpainting
-torch.manual_seed(0)  # Set seed for reproducibility
-noise = torch.randn_like(latents)
+    scheduler.set_timesteps(25)
+    guidance_scale = 7 # Control the CFG. Higher values -> higher dependency on the prompt. Values should be around 6-12.
+    optimization_scale = 0.5
 
-# Applying the diffusion iterations.
-for t in scheduler.timesteps:
-    latent_model_input = scheduler.scale_model_input(latents, t) # Note: scale_model_input actually do nothing with current schedule, but safer. 
+    noise = torch.randn_like(latents)
+
+    # Applying the diffusion iterations.
+    for i, t in enumerate(scheduler.timesteps):
+        # 'prev_latent' will be the previous latent of the loop. It will not be touched thourghtout the loop.
+        # 'latents' will be the current latent representation, which will be changed and pass to the next iteration.
+        prev_latent = latents # Note: scale_model_input actually do nothing with current schedule, but safer. 
+
+        with torch.no_grad():
+            noise_pred_text = unet(
+                prev_latent,
+                t,
+                encoder_hidden_states=text_embeddings
+            ).sample
+            noise_pred_uncond = unet(
+                prev_latent, 
+                t, 
+                encoder_hidden_states=uncond_embeddings
+            ).sample
+
+        # Apply Classifier Free Guidance
+        noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
+
+        latents = scheduler.step( # Denoise with Classifier Free Guidance noise
+            noise_pred, t, prev_latent
+        ).prev_sample
+
+        # Forward-noise the original latent to the current timestep t
+        latents_original_image_noised = scheduler.add_noise(
+            latent_original_image,
+            noise,
+            t
+        )
+        
+        gamma = 0.005
+        # Apply latent space optimization with the loss function
+        latents = latents.detach().requires_grad_(True)
+        loss = loss_fn(
+            gamma=gamma,
+            diffusion_latent=latents, 
+            original_image_latent=latents_original_image_noised,
+            mask=mask_latent,
+            mask_edge=mask_edge_latent
+        )
+
+        loss_before = loss
+
+        grad = torch.autograd.grad(loss, latents)[0]
+        with torch.no_grad():
+            latents = latents - optimization_scale * grad
+
+        # ----- loss after step -----
+        latents_updated = latents
+        loss_after = loss_fn(
+            gamma=gamma,
+            diffusion_latent=latents_updated,
+            original_image_latent=latents_original_image_noised,
+            mask=mask_latent,
+            mask_edge=mask_edge_latent
+        )
+
+        print(f"Loss before: {loss_before.item():.6f}")
+        print(f"Loss after : {loss_after.item():.6f}")
+        print(f"Improvement: {(loss_before - loss_after).item():.6f}")
+        
+        latents = latents.detach()
+
+    latents = latents / vae.config.scaling_factor
 
     with torch.no_grad():
-        noise_pred_text = unet(
-            latent_model_input,
-            t,
-            encoder_hidden_states=text_embeddings
-        ).sample
-        noise_pred_uncond = unet(
-            latent_model_input, 
-            t, 
-            encoder_hidden_states=uncond_embeddings
-        ).sample
+        image = vae.decode(latents).sample
 
-    # Apply Classifier Free Guidance
-    noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
+    image = utils.tensor_to_image(image_tensor=image)
+    images.append(image)
+    image.save("../data/outputs/main_outputs/inpaint0" + str(image_num) + str(inpaint_iteration) + ".jpg")
 
-    latents = scheduler.step(
-        noise_pred, t, latents
-    ).prev_sample
-
-    # Forward-noise the original latent to the current timestep t
-    latents_original_image_noised = scheduler.add_noise(
-        latent_original_image,
-        noise,
-        t
-    )
-    
-    # Apply latent space optimization with the loss function
-    latents = latents.detach().requires_grad_(True)
-    loss = loss_fn(diffusion_latent=latents, original_image_latent=latents_original_image_noised, mask=mask_latent_tensor)
-    grad = torch.autograd.grad(loss, latents)[0]
-    with torch.no_grad():
-        latents = latents - optimization_scale * grad
-    latents = latents.detach()
-
-    # Clamp known region. Note: '*' is elemnent-wise multiplication.
-    latents = mask_latent_tensor * latents_original_image_noised + (1 - mask_latent_tensor) * latents
-
-latents = latents / vae.config.scaling_factor
-
-with torch.no_grad():
-    image = vae.decode(latents).sample
-
-utils.tensor_to_image(image_tensor=image, saving_path=saving_output_path)
+clip_score = utils.CLIPScore(device, dtype)
+CLIP_scores = [clip_score.CLIP_score(img, prompt) for img in images]
+best_index = CLIP_scores.index(max(CLIP_scores))
+best_CLIP_image = images[best_index]
+best_CLIP_image.save(saving_output_path)
